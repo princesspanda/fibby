16734a293aeb575acc0402230784cdc7408ca497
Amy Chen <lemur.amychen@gmail.com>
2011-02-28 23:32:56 -0800

    Two ways to skin this cat -- you can calculate the fibonacci numbers using an iteratve approach or generator approach.

--- lib/fibby.rb
+++ lib/fibby.rb
@@ -1,18 +1,36 @@
 module Fibby
 
   def closest_fibonacci
-    fib1 = 0
-    fib2 = 1
-
     raise NoFibExistsError.new if self < 1
 
-    while fib2 < self
-      next_fib = fib1 + fib2
-      fib1 = fib2
-      fib2 = next_fib
-    end
-    fib1
+    # Two options here -- the iterative or generative approach
+    iterative_fib
+#    fib_using_generator
+  end
 
+  private
+
+  def iterative_fib
+    f1, f2 = 0, 1
+
+    while f2 < self
+      f1, f2 = f2, f1+f2
+    end
+    f1
+  end
+
+  def fib_using_generator
+    fib_generator do |f1, f2|
+      return f1 if f2 > self
+    end
+  end
+
+  def fib_generator
+    f1, f2 = 0, 1
+    while true
+      yield f1, f2
+      f1, f2 = f2, f1+f2
+    end
   end
 
 
